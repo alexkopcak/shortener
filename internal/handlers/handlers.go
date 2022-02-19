5217e0842d8475d33269e6c80bf846848d21177f
alex.kopcak <alex.kopcak@gmail.com>
2022-02-19 10:17:38 +0300

    Remove get long URL value form context

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,20 +1,16 @@
 package handlers
 
 import (
-	"context"
+	"fmt"
 	"io"
 	"net/http"
-	"strings"
 
 	"github.com/alexkopcak/shortener/internal/storage"
 	"github.com/go-chi/chi/v5"
 )
 
-type key int
-
 const (
-	serverAddr         = "http://localhost:8080/"
-	keyPrincipalID key = iota
+	serverAddr = "http://localhost:8080/"
 )
 
 type Handler struct {
@@ -28,10 +24,7 @@ func URLHandler(repo *storage.Dictionary) *Handler {
 		Repo: *repo,
 	}
 
-	h.Mux.Route("/{idValue}", func(r chi.Router) {
-		r.Use(h.IDContext)
-		r.Get("/", h.GetHandler())
-	})
+	h.Mux.Get("/{idValue}", h.GetHandler())
 	h.Mux.Post("/", h.PostHandler())
 	h.Mux.MethodNotAllowed(h.MethodNotAllowed())
 	h.Mux.NotFound(h.NotFound())
@@ -51,31 +44,17 @@ func (h *Handler) NotFound() http.HandlerFunc {
 	}
 }
 
-func (h *Handler) IDContext(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) GetHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		idValue := chi.URLParam(r, "idValue")
-		if idValue == "" ||
-			strings.TrimSpace(idValue) == "" {
+		fmt.Println("***", idValue)
+		if idValue == "" {
 			http.Error(w, "Bad request!", http.StatusBadRequest)
 			return
 		}
 		longURLValue := h.Repo.GetURL(idValue)
 		if longURLValue == "" {
 			http.Error(w, "There are no any short Urls", http.StatusBadRequest)
-			return
-		}
-
-		ctx := context.WithValue(r.Context(), keyPrincipalID, longURLValue)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
-func (h *Handler) GetHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		longURLValue, ok := ctx.Value(keyPrincipalID).(string)
-		if !ok {
-			http.Error(w, "Something went wrong", http.StatusBadRequest)
 			return
 		}
 		w.Header().Set("Location", longURLValue)
