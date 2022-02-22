c13448831016ac99e32d6bcb16668a49b9eb499f
alex.kopcak <alex.kopcak@gmail.com>
2022-02-22 18:00:48 +0300

    add flag options

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -22,6 +23,15 @@ func Run() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// flags configuration
+	addrPointer := flag.String("a", cfg.ServerAddr, "Server address, example ip:port")
+	baseAddrPointer := flag.String("b", cfg.BaseURL, "Base URL address, example http://127.0.0.1:8080")
+	fileStoragePathPointer := flag.String("f", cfg.FileStoragePath, "File storage path")
+	flag.Parse()
+
+	cfg.BaseURL = *baseAddrPointer
+	cfg.ServerAddr = *addrPointer
+	cfg.FileStoragePath = *fileStoragePathPointer
 
 	// Repository
 	dictionary := storage.NewDictionary(cfg.FileStoragePath)
