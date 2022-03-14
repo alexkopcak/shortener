e24fa4677f02c0d554f12b44eb6805bcd21ddcc6
alex.kopcak <alex.kopcak@gmail.com>
2022-03-14 11:17:57 +0300

    add db connection string

--- internal/app/app.go
+++ internal/app/app.go
@@ -20,12 +20,13 @@ func Run() error {
 	addrPointer := flag.String("a", cfg.ServerAddr, "Server address, example ip:port")
 	baseAddrPointer := flag.String("b", cfg.BaseURL, "Base URL address, example http://127.0.0.1:8080")
 	fileStoragePathPointer := flag.String("f", cfg.FileStoragePath, "File storage path")
+	dbConnectionString := flag.String("d", cfg.DBConnectionString, "DB Connection string")
 	flag.Parse()
 
 	cfg.BaseURL = *baseAddrPointer
 	cfg.ServerAddr = *addrPointer
 	cfg.FileStoragePath = *fileStoragePathPointer
-
+	cfg.DBConnectionString = *dbConnectionString
 	// Repository
 	dictionary, err := storage.NewDictionary(cfg.FileStoragePath)
 	if err != nil {
--- internal/app/config.go
+++ internal/app/config.go
@@ -1,9 +1,10 @@
 package app
 
 type config struct {
-	ServerAddr      string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-	SecretKey       string `env:"SHORTENER_SECRET_KEY" envDefault:"We learn Go language"`
-	CookieAuthName  string `env:"COOKIE_ATUH_NAME" envDefault:"shortenerId"`
+	ServerAddr         string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
+	BaseURL            string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	FileStoragePath    string `env:"FILE_STORAGE_PATH"`
+	SecretKey          string `env:"SHORTENER_SECRET_KEY" envDefault:"We learn Go language"`
+	CookieAuthName     string `env:"COOKIE_ATUH_NAME" envDefault:"shortenerId"`
+	DBConnectionString string `env:"DATABASE_DSN"`
 }
