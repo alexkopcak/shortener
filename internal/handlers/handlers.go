55dd2f1a7bf41fe9a8e40e35c9d2c9ce4b233503
alex.kopcak <alex.kopcak@gmail.com>
2022-05-30 19:49:12 +0300

    add url duplicate variable

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -326,21 +326,21 @@ func (h *Handler) PostAPIHandler() http.HandlerFunc {
 		}
 
 		requestValue, err := h.Repo.AddURL(r.Context(), aliasRequest.LongURLValue, userID)
+		duplicate := false
+		if errors.Is(err, storage.ErrDuplicateRecord) {
+			duplicate = true
+			err = nil
+		}
 		if err != nil {
-			if errors.Is(err, storage.ErrDuplicateRecord) {
-				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusConflict)
-				return
-			}
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
-		// if errors.Is(err, storage.ErrDuplicateRecord) {
-		// 	w.WriteHeader(http.StatusConflict)
-		// } else {
-		w.WriteHeader(http.StatusCreated)
-		// }
+		if duplicate {
+			w.WriteHeader(http.StatusConflict)
+		} else {
+			w.WriteHeader(http.StatusCreated)
+		}
 
 		responseValue := struct {
 			ResultValue string `json:"result"`
@@ -378,23 +378,22 @@ func (h *Handler) PostHandler() http.HandlerFunc {
 
 		//		fmt.Printf("userID: %v\n", userID)
 		requestValue, err := h.Repo.AddURL(r.Context(), aliasRequest.LongURLValue, userID)
+		duplicate := false
+		if errors.Is(err, storage.ErrDuplicateRecord) {
+			duplicate = true
+			err = nil
+		}
 		if err != nil {
-			if errors.Is(err, storage.ErrDuplicateRecord) {
-				w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-				w.WriteHeader(http.StatusConflict)
-				w.Write([]byte(h.Cfg.BaseURL + "/" + requestValue))
-				return
-			}
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		// w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		// if errors.Is(err, storage.ErrDuplicateRecord) {
-		// 	w.WriteHeader(http.StatusConflict)
-		// } else {
-		w.WriteHeader(http.StatusCreated)
-		//		}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		if duplicate {
+			w.WriteHeader(http.StatusConflict)
+		} else {
+			w.WriteHeader(http.StatusCreated)
+		}
 		_, err = w.Write([]byte(h.Cfg.BaseURL + "/" + requestValue))
 		if err != nil {
 			http.Error(w, "Something went wrong!", http.StatusBadRequest)
