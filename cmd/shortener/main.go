f9b6e1f9b687e5a99521fc345f6ef3a03eda2140
alex.kopcak <alex.kopcak@gmail.com>
2022-06-14 21:43:03 +0300

    link flags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/alexkopcak/shortener/internal/app"
 )
 
+// link flags
+var (
+	buildVersion string = "N/A" // -X 'main.buildVersion=v1.0.1'
+	buildDate    string = "N/A" // -X 'main.buildDate=$(date +'%Y/%m/%d %H:%M:%S')'
+	buildCommit  string = "N/A" // -X 'main.buildCommit=$(git show -s --format=%s)'
+)
+
 func main() {
+	fmt.Println("Build version:", buildVersion)
+	fmt.Println("Build date:", buildDate)
+	fmt.Println("Build commit:", buildCommit)
 	log.Fatal(app.Run())
 }
